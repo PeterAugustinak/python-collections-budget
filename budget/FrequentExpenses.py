0562a1c423e256924fe5b99837e94543d3df98f8
p-a <peter.augustinak@gmail.com>
2022-01-14 19:12:10 +0100

    Count Expenses by Category - fixed variable name counts -> count

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -16,9 +16,9 @@ spending_counter = collections.Counter(spending_categories)
 # print(spending_counter)
 
 top5 = spending_counter.most_common(5)
-categories, counts = zip(*top5)
+categories, count = zip(*top5)
 
 fig, ax = plt.subplots()
-ax.bar(categories, counts)
+ax.bar(categories, count)
 ax.set_title("# of Purchases by Category")
 plt.show()
